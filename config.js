202392ee2d6ccf58ccc0d5c8ef99252b0ceb94f1
thinnguyen10042000 <84697051+thinnguyen10042000@users.noreply.github.com>
2021-06-22 13:00:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Em yêu ơiiii!',
+    introTitle: 'Em yêu ơiiiii!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
