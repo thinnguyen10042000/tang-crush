3cf6ae92fcf637cf0cfcdb6a7f8c825a12ccb777
thinnguyen10042000 <84697051+thinnguyen10042000@users.noreply.github.com>
2021-08-08 21:44:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Em yêu ơiiiii!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Em biết không. Khi em cười với tôi 
+    -Thì tôi cứ như hoà làm 1 với Pu-skin 
+    -Vì trong đầu tôi lúc đó đã hiện lên một tuyệt tác mang tên "Tôi yêu em"`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã jhgyug ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là yêu anh nhiềuu <3',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu anh nhiều nhiềuu <3',
     btnNo: 'Không, yêu ít à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anhh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
+    reply: 'Tại vì em yêu anh! Thế thôi :3333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Công chúa xíu ngủ ngon nhaaa <3333.',
     btnAccept: 'Okiee lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100012736872719/' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://www.facebook.com/messages/t/100012736872719/' 
